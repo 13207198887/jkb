9737472f7a5f7b7278cca5c1aaf79738b6faa436
13207198887 <38848123+13207198887@users.noreply.github.com>
2021-03-03 12:46:37 +0800

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,7 +1,7 @@
+# -*- coding:utf-8 -*-
 import requests
 
-burp0_url = "http://www.bugzero.cn:80/a/login"
-burp0_cookies = {"Hm_lvt_cf49a60363f5b0477076111dbd3a4b90": "1601541967,1602941023", "pageNo": "1", "pageSize": "12", "JSESSIONID": "2B82120BDF5AA392191B46DE5A8BD2B0", "jeesite.session.id": "f6b3e6c91b97439d93d56bb0a422b70e", "Hm_lpvt_cf49a60363f5b0477076111dbd3a4b90": "1602971883"}
-burp0_headers = {"Accept": "application/json, text/javascript, */*; q=0.01", "Origin": "http://www.bugzero.cn", "X-Requested-With": "XMLHttpRequest", "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36", "DNT": "1", "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8", "Referer": "http://www.bugzero.cn/index/login.html", "Accept-Encoding": "gzip, deflate", "Accept-Language": "zh,zh-CN;q=0.9", "Connection": "close"}
-burp0_data = {"username": "17095936094", "password": "qq960201567", "validateCode": ''}
-requests.post(burp0_url, headers=burp0_headers, cookies=burp0_cookies, data=burp0_data)
+burp0_url = "http://bugzero.cn:80/a/login"
+burp0_cookies = {"jeesite.session.id": "c74fe9b853d442ae800eab58449b115b", "rememberMe": "deleteMe", "Hm_lvt_cf49a60363f5b0477076111dbd3a4b90": "1600862681", "Hm_lpvt_cf49a60363f5b0477076111dbd3a4b90": "1602965724", "JSESSIONID": "D2A6565D8D8ADFB6BDC96639FDA26F3A"}
+burp0_headers = {"Accept": "application/json, text/javascript, */*; q=0.01", "X-Requested-With": "XMLHttpRequest", "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36", "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8", "Origin": "http://bugzero.cn", "Referer": "http://bugzero.cn/index/login.html", "Accept-Encoding": "gzip, deflate", "Accept-Language": "zh-CN,zh;q=0.9", "Connection": "close"}
+requests.post(burp0_url, headers=burp0_headers, cookies=burp0_cookies)
