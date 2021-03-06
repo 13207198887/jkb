20dd9da9ba48084f9c6f5f321b3861c77a720ab8
13207198887 <38848123+13207198887@users.noreply.github.com>
2021-03-06 12:09:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@
 import requests
 
 burp0_url = "http://bugzero.cn:80/a/login"
-burp0_cookies = {"jeesite.session.id": "075203d03eb041178e0d232008c5675c", "rememberMe": "deleteMe", "Hm_lvt_cf49a60363f5b0477076111dbd3a4b90": "1600862681", "Hm_lpvt_cf49a60363f5b0477076111dbd3a4b90": "1602965724", "JSESSIONID": "D2A6565D8D8ADFB6BDC96639FDA26F3A"}
+burp0_cookies = {"jeesite.session.id": "fa02a7f7ca7e4d4fb8684c2c237c10d0", "rememberMe": "deleteMe", "Hm_lvt_cf49a60363f5b0477076111dbd3a4b90": "1600862681", "Hm_lpvt_cf49a60363f5b0477076111dbd3a4b90": "1602965724", "JSESSIONID": "D2A6565D8D8ADFB6BDC96639FDA26F3A"}
 burp0_headers = {"Accept": "application/json, text/javascript, */*; q=0.01", "X-Requested-With": "XMLHttpRequest", "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36", "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8", "Origin": "http://bugzero.cn", "Referer": "http://bugzero.cn/index/login.html", "Accept-Encoding": "gzip, deflate", "Accept-Language": "zh-CN,zh;q=0.9", "Connection": "close"}
 requests.post(burp0_url, headers=burp0_headers, cookies=burp0_cookies)
